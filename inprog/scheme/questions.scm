88f5d0e82ec836cfc4dbae235d6b99a8cf283b59
DevonMartin <85790366+DevonMartin@users.noreply.github.com>
2022-09-09 15:17:41 -0400

    Finished Scheme, other than optionals

--- inprog/scheme/questions.scm
+++ inprog/scheme/questions.scm
@@ -61,18 +61,42 @@
 )
   ; END PROBLEM 16
 
-
-(merge < '(1 5 7 9) '(4 8 10))
-; expect (1 4 5 7 8 9 10)
-(merge > '(9 7 5 1) '(10 8 4 3))
-; expect (10 9 8 7 5 4 3 1)
-
 ;; Problem 17
 
 (define (nondecreaselist s)
     ; BEGIN PROBLEM 17
-    'replace-this-line
+    (cond
+        ((not (null? s))
+            (define new_list '())
+            (define tmp '(()))
+            (define last_num (- (car s) 1))
+            (define (helper_func s new_list tmp last_num) 
+                (cond
+                    ((not (null? s))
+                        (cond
+                            ((not (< (car s) last_num))
+                                (define tmp (list (append (car tmp) (list (car s)))))
+                            )
+                            (else
+                                (define new_list (append new_list tmp))
+                                (define tmp (list (list (car s))))
+                            )
+                        )
+                        (define last_num (car s))
+                        (define s (cdr s))
+                        (helper_func s new_list tmp last_num)
+                    )
+                    (else
+                        (define new_list (append new_list tmp)) 
+                        new_list
+                    )
+                )
+            )
+            (helper_func s new_list tmp last_num)
+        )
+        (else s)
     )
+)
     ; END PROBLEM 17
 
 
