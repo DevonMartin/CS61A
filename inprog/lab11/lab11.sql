c8e929278609a6c35a4d9f62043f016105c0611c
DevonMartin <85790366+DevonMartin@users.noreply.github.com>
2022-09-12 15:13:28 -0400

    Finished q 01-04

--- inprog/lab11/lab11.sql
+++ inprog/lab11/lab11.sql
@@ -1,14 +1,22 @@
 .read sp20data.sql
 
 CREATE TABLE obedience AS
-  SELECT seven, instructor 
-         FROM students;
+      SELECT seven, instructor 
+        FROM students;
 
 CREATE TABLE smallest_int AS
-  SELECT "REPLACE THIS LINE WITH YOUR SOLUTION";
+      SELECT time, smallest 
+        FROM students 
+       WHERE smallest > 2 
+       ORDER BY smallest 
+       LIMIT 20;
 
 CREATE TABLE matchmaker AS
-  SELECT "REPLACE THIS LINE WITH YOUR SOLUTION";
+  SELECT a.pet, a.song, a.color, b.color
+    FROM students as a, students as b
+   WHERE a.pet = b.pet 
+     AND a.song = b.song
+     AND a.time < b.time;
 
 CREATE TABLE parents AS
   SELECT "abraham" AS parent, "barack" AS child UNION
